db11c513365210c85bbfee62d84a9757fa41d625
Hadi <hdtrn@gmail.com>
2017-09-18 21:47:01 -0700

    yikesyikesyikes trying toget rid of http 400 error

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -17,25 +17,22 @@ async def on_message(message):
     if message.content.startswith("!wiki"):
         query = message.content[6:]
         #await client.send_message(message.channel, "copy that: " + query)
-        data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=xml&titles=" + query).text
+        data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+query).text
         if '#REDIRECT' in data:
-            query = re.search(r"\[\[(.+?)\]\]", data).group(1)
-            await client.send_message(query)
-            data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + query).text
+            await client.send_message("data[re.search(r'\[\[', data)+1:re.search(r'\]\]', data)-1])
+            data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + data[re.search(r'\[\[', data)+1:re.search(r'\]\]', data)-1]).text
         desc = data
         data = requests.post(url='http://conwaylife.com/w/api.php', headers={'Connection':'close'})
-        pgtitle = re.search(r'title="(.+?)">', desc).group(1)
-        
         desc = re.sub(r"'''", r"**", desc)
         desc = re.sub(r" \(.+?\)", r"", desc)
         desc = re.sub(r"<.+?>", r"", desc)
-        desc = re.sub(r"\[\[:Category:(.+?)\]\]", r"\1", desc)
+        desc = re.sub(r"\[\[:Category:.+?\|(.+?)\]\]", r"\1", desc)
         desc = re.sub(r"\[\[(.*?)(\|)?(?(2).*?)\]\]", r"\1", desc)
         desc = re.sub(r"{.+?}}", r"", desc)
         desc = re.sub(r"\\.", r"", desc)
         desc = re.sub(r"=.*", r"", desc)
         #await client.send_message(message.channel, "desc: " + desc)
-        embed = discord.Embed(title=pgtitle, description=desc, url="http://conwaylife.com/wiki/" + query, color=0x680000)
+        embed = discord.Embed(title=query, description=desc, color=0x680000)
         await client.send_message(message.channel, embed=embed)
 
 client.run('MzU5MDY3NjM4MjE2Nzg1OTIw.DKBnUw.MJm4R_Zz6hCI3TPLT05wsdn6Mgs')
