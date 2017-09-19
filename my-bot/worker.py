4d91655eda45f01a6c1a192908a1b2a16de59530
Hadi <hdtrn@gmail.com>
2017-09-18 21:53:28 -0700

    implementing proper page title rendering and hotlinked embed

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -20,7 +20,7 @@ async def on_message(message):
         data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+query).text
         if '#REDIRECT' in data:
             query = re.search(r'\[\[(.+?)\]\]', data).group(1)
-            await client.send_message(query)
+            await client.send_message(message.channel, query)
             data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + query).text
         desc = data
         data = requests.post(url='http://conwaylife.com/w/api.php', headers={'Connection':'close'})
@@ -36,7 +36,7 @@ async def on_message(message):
         desc = re.sub(r"\\.", r"", desc)
         desc = re.sub(r"=.*", r"", desc)
         #await client.send_message(message.channel, "desc: " + desc)
-        embed = discord.Embed(title=query, description=desc, color=0x680000)
+        embed = discord.Embed(title=pgtitle, url='http://conwaylife.com/wiki/'+pgtitle, description=desc, color=0x680000)
         await client.send_message(message.channel, embed=embed)
 
 client.run('MzU5MDY3NjM4MjE2Nzg1OTIw.DKBnUw.MJm4R_Zz6hCI3TPLT05wsdn6Mgs')
