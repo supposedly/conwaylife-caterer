74f837cd847019cc9b37ff2fabd15f26e6febc34
Hadi <hdtrn@gmail.com>
2017-09-18 20:32:27 -0700

    more regex

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -16,7 +16,7 @@ async def on_ready():
 async def on_message(message):
     if message.content.startswith("test"):
         query = message.content[5:]
-        await client.send_message(message.channel, "copy that: " + query)
+        #await client.send_message(message.channel, "copy that: " + query)
         data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles="+query).text
         if '#REDIRECT' in data:
             data = requests.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + data[re.search(r'\[\[', data)+1:re.search(r'\]\]', data)-1]).text
@@ -28,7 +28,7 @@ async def on_message(message):
         desc = re.sub(r"{.+?}}", r"", desc)
         desc = re.sub(r"\\.", r"", desc)
         desc = re.sub(r"=.*", r"", desc)
-        await client.send_message(message.channel, "desc: " + desc)
+        #await client.send_message(message.channel, "desc: " + desc)
         embed = discord.Embed(title=query, description=desc, color=0x680000)
         await client.send_message(message.channel, embed=embed)
 
