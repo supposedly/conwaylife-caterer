f229d09225cc9911a69b65bd1a6bae82c35688c6
Hadi <hdtrhn@gmail.com>
2017-09-22 16:18:16 -0700

    refactored to make disambig easier +incorporating prev changes

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -39,6 +39,31 @@ def regex(txt):
         txt = '**' + txt
     return txt
 
+def regpage(data, query, rqst):
+    images = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=images&format=json&titles=" + query).text
+    pgimg = rgif.search(images)
+    find = rimage.findall(images)
+    pgimg = (pgimg.group(0) if pgimg else (min(find, key = len) if find else ''))
+    images = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=" + pgimg).text
+    pgimg = rfileurl.search(images)
+    if pgimg:
+        pgimg = pgimg.group(1)
+        em.set_thumbnail(url=pgimg)
+
+    pgtitle = rtitle.search(data).group(1)
+    desc = unescape(regex(data))
+
+    em.title = pgtitle
+    em.url = "http://conwaylife.com/wiki/" + query.replace(" ", "_")
+    em.description = desc
+    em.color = 0x680000
+
+def disambiguation():
+    return
+    
+def reactions():
+    return
+
 client = discord.Client()
 
 @client.event
@@ -50,40 +75,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    em = discord.Embed()
     if message.content.startswith("!wiki"):
+        em = discord.Embed()
+        disambig = 0
         query = message.content[6:]
-        
         with requests.Session() as rqst:
             data = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + query).text
             
+            if '#REDIRECT' in data:
+                em.set_footer(text='(redirected from "' + query + '")')
+                query = rredirect.search(data).group(1)
+                data = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + query).text
+                
             if '"-1":{' in data:
                 await client.send_message(message.channel, 'Page `' + query + '` does not exist.')
             else:
-                if '#REDIRECT' in data:
-                    em.set_footer(text='(redirected from "' + query + '")')
-                    query = rredirect.search(data).group(1)
-                    data = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=" + query).text
-                    
-                images = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=images&format=json&titles=" + query).text
-                pgimg = rgif.search(images)
-                find = rimage.findall(images)
-                pgimg = (pgimg.group(0) if pgimg else (min(find, key = len) if find else ''))
-                images = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=" + pgimg).text
-                pgimg = rfileurl.search(images)
-                if pgimg:
-                    pgimg = pgimg.group(1)
-                    em.set_thumbnail(url=pgimg)
-                    
-                pgtitle = rtitle.search(data).group(1)
-                desc = unescape(regex(data))
-            
-                em.title = pgtitle
-                em.url = "http://conwaylife.com/wiki/" + query.replace(" ", "_")
-                em.description = desc
-                em.color = 0x680000
-            
+                if "(disambiguation)" in data:
+                    disambig = 1
+                
+                regpage(data, query, rqst)            
                 await client.send_message(message.channel, embed=em)
 
 client.run('MzU5MDY3NjM4MjE2Nzg1OTIw.DKBnUw.MJm4R_Zz6hCI3TPLT05wsdn6Mgs')
-
