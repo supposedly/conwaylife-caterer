d790c4bfbfcb45f1df96faf71b2bbf58127cf1af
Hadi <hdtrhn@gmail.com>
2017-09-23 20:22:08 -0700

    debug

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -46,6 +46,7 @@ def regex(txt):
 #   txt = rfirstheader.sub('', txt)
 
     fixbold = txt.find('**')
+    print(txt)
     if txt[fixbold+2] == ' ' or txt[fixbold+2] == ',':
         txt = '**' + txt
     return txt
