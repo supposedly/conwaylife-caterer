2ddca1a82d966f6f2466aecebec299a94094d6f5
eltrhn <32081933+eltrhn@users.noreply.github.com>
2017-10-08 06:54:22 -0700

    Hmmm

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -84,7 +84,7 @@ async def on_message(message):
 
     if message.content.startswith("!invite" if message.server.id == '357922255553953794' else "ca.invite"):
         em = discord.Embed(url=discord.utils.oauth_url(client.user.id, permissions=discord.Permissions(permissions=52288)), title='Use this link to invite me to your server!', color=0x000000)
-        em.set_image(client.user.avatar_url)
+        em.set_image(url=client.user.avatar_url)
         await client.send_message(message.channel, embed=em)
     
     if message.content.startswith("!wiki" if message.server.id == '357922255553953794' else "ca.wiki"):
