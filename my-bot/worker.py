970937f8805d5ea169ebeabcb92a6121ecccbd67
eltrhn <32081933+eltrhn@users.noreply.github.com>
2017-10-08 23:35:30 -0700

    More fix, this time utf-32→16

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -129,7 +129,7 @@ Commands:
     if message.content.startswith(prefix + "wiki"):
         query = message.content[1+message.content.find(' '):]
         if query[:1].lower() + query[1:] == "caterer":
-            await message.add_reaction(u'\uD83D\uDC4B')
+            await message.add_reaction(u'\u0001\uF44B')
         await message.channel.trigger_typing()
     
         em = discord.Embed()
