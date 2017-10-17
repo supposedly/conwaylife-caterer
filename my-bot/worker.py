6cba1c747066c338cff05a29a4c9ad4cb7483e9e
Hadi <hdtrhn@gmail.com>
2017-10-16 17:10:30 -0700

    introduced directories n stuff and also converted all instances of 'str' + var to f'str{var}'

--- my-bot/worker.py
+++ my-bot/worker.py
@@ -15,12 +15,20 @@ def get_prefix(bot, message):
 bot = commands.Bot(command_prefix=get_prefix, description="A 'caterer' bot for the cellular automata community's Discord server")
 bot.remove_command('help')
 
+cogs = ['invite', 'wiki', 'help', 'sim']
+
 @bot.event
 async def on_ready():
+    if __name__ == '__main__':
+        for cog in cogs:
+            try:
+                bot.load_extension(cog)
+            except Exception as e:
+                print(f"Loading of command '{cog}' failed.")
     global oauth
     oauth = discord.utils.oauth_url(bot.user.id, permissions=discord.Permissions(permissions=388160))
 #   https://discordapp.com/oauth2/authorize?client_id=359067638216785920&scope=bot&permissions=388160
-    print('Discord: ' + discord.__version__)
+    print(f'Discord: {discord.__version__}')
     print('Logged in as')
     print(bot.user.name)
     print(bot.user.id)
@@ -59,11 +67,11 @@ def parse(txt):
     return txt
 
 def regpage(data, query, rqst, em):
-    images = rqst.get("http://conwaylife.com/w/api.php?action=query&prop=images&format=json&titles=" + query).text
+    images = rqst.get(f'http://conwaylife.com/w/api.php?action=query&prop=images&format=json&titles={query}').text
     pgimg = rgif.search(images)
     find = rimage.findall(images)
     pgimg = (pgimg.group(0) if pgimg else (min(find, key = len) if find else ''))
-    images = json.loads(rqst.get("http://conwaylife.com/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles=" + pgimg).text)
+    images = json.loads(rqst.get(f'http://conwaylife.com/w/api.php?action=query&prop=imageinfo&iiprop=url&format=json&titles={pgimg}').text)
     try:
         pgimg = list(images["query"]["pages"].values())[0]["imageinfo"][0]["url"]
         em.set_thumbnail(url=pgimg)
@@ -74,7 +82,7 @@ def regpage(data, query, rqst, em):
     desc = unescape(parse(data["parse"]["text"]["*"]))
 
     em.title = pgtitle
-    em.url = "http://conwaylife.com/wiki/" + pgtitle.replace(" ", "_")
+    em.url = f'http://conwaylife.com/wiki/{pgtitle.replace(" ", "_")}'
     em.description = desc
 
 def parsedisambig(txt):
@@ -92,7 +100,7 @@ def parsedisambig(txt):
 def disambig(data):
     pgtitle = data["parse"]["title"]
     desc_links = parsedisambig(data["parse"]["text"]["*"])
-    return (discord.Embed(title=pgtitle, url='http://conwaylife.com/wiki/' + pgtitle.replace(' ', '_'), description=desc_links[0], color=0xffffff), desc_links[1])
+    return (discord.Embed(title=pgtitle, url=f'http://conwaylife.com/wiki/{pgtitle.replace(" ", "_")}', description=desc_links[0], color=0xffffff), desc_links[1])
 
 
 @bot.command()
@@ -115,7 +123,7 @@ Commands:
             await ctx.send(embed=em)
     
 
-@bot.command(description='Produce an invite link for this bot')
+@bot.command(description="Produce an invite link for this bot. Not in its own cog bc it's easier to have it in the same file as the oauth var (read: im lazy)")
 async def invite(ctx):
     em = discord.Embed(description='Use [this link](' + oauth + ') to add me to your server!', color=0x000000)
     em.set_author(name='Add me!', icon_url=bot.user.avatar_url)
@@ -132,7 +140,7 @@ async def wiki(ctx, *, query: str):
         edit = False
         
         if query[:1].lower() + query[1:] == 'methusynthesis':
-            em.set_footer(text='(redirected from "' + query + '")')
+            em.set_footer(text=f'(redirected from "{query}")')
             query = "methusynthesae"
         if query[:1].lower() + query[1:] == 'methusynthesae':
             gus = "**Methusynthesae** are patterns/methuselah that basically/mildly are spaceship reactions, though it is a bit hard to explain the relation. It is way different from syntheses because they are patterns, and don't form other patterns."
@@ -143,17 +151,17 @@ async def wiki(ctx, *, query: str):
             await ctx.send(embed=em)
         else:
             with requests.Session() as rqst:
-                data = rqst.get("http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page=" + query).text
+                data = rqst.get(f'http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page={query}').text
                 
                 if '>REDIRECT ' in data:
                     em.set_footer(text='(redirected from "' + query + '")')
                     query = rredirect.search(data).group(1)
-                    data = rqst.get("http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page=" + query).text
+                    data = rqst.get(f'http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page={query}').text
                     
                 if 'missingtitle' in data:
                     await ctx.send('Page `' + query + '` does not exist.') # no sanitization yeet
                 if 'invalidtitle' in data:
-                    await ctx.send('Invalid title: `' + query + '`')
+                    await ctx.send(f'Invalid title: `{query}`')
                 else:
                     data = json.loads(data)
                     if "(disambiguation)" in data["parse"]["title"]:
@@ -172,7 +180,7 @@ async def wiki(ctx, *, query: str):
                             await msg.clear_reactions()
                             return
                         query = links[numbers_fu.index(react.emoji)]
-                        data = json.loads(rqst.get("http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page=" + query).text)
+                        data = json.loads(rqst.get(f'http://conwaylife.com/w/api.php?action=parse&prop=text&format=json&section=0&page={query}').text)
                     
                     regpage(data, query, rqst, em)
                     if edit:
