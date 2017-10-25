2acba1cf543811a7ac5654fe87e6a23d499bac77
Hadi <hdtrhn@gmail.com>
2017-10-24 17:54:49 -0700

    convert to absolute path + cat statements for potential debugging

--- cogs/ca.py
+++ cogs/ca.py
@@ -77,8 +77,9 @@ class CA:
         
         with open(f'{ctx.message.id}_in.rle', 'w') as patfile:
             patfile.write(parse["pat"])
+            filedir = os.path.dirname(os.path.abspath(patfile))
         
-        os.system('{0}/resources/bgolly -m {1[gen]} -i {1[step]} -q -q -r {1[rule]} -o {2}_out.rle {2}_in.rle'.format(os.path.dirname(__file__), parse, ctx.message.id))
+        os.system('{0}/resources/bgolly -m {1[gen]} -i {1[step]} -q -q -r {1[rule]} -o {0}/{2}_out.rle {3}/{2}_in.rle && cat {0}/{2}_out.rle && cat {3}/{2}_in.rle'.format(os.path.dirname(os.path.abspath(__file__)), parse, ctx.message.id, filedir))
         # From here:
         # readlines on bgolly's output file and divide resulting list into two - one with each individual RLE and one with corresponding (width, height)
         # for pattern in rle_list: turn into .lif file with final regexes above
