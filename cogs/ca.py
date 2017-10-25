bbc2276b080d8f663ccadbfa6c61fe8e533fc70e
Hadi <hdtrhn@gmail.com>
2017-10-24 21:20:38 -0700

    fixed filepath again

--- cogs/ca.py
+++ cogs/ca.py
@@ -84,7 +84,7 @@ class CA:
         os.system('{0}/resources/bgolly -m {1[gen]} -i {1[step]} -q -q -r {1[rule]} -o {0}/{2}_out.rle {3}/{2}_in.rle'.format(self.file, parse, ctx.message.id, filedir))
         # From here:
         # readlines on bgolly's output file and divide resulting list into two - one with each individual RLE and one with corresponding (width, height)
-        with open(f'{ctx.message.id}_out.rle', 'r') as pat:
+        with open(f'{self.file}/{ctx.message.id}_out.rle', 'r') as pat:
             patlist = [line.rstrip('\n') for line in pat]
         
         headers = patlist[::2] # just headers
