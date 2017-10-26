7a176dde2d3347f3f856d3260304ebf28190f774
Hadi <hdtrhn@gmail.com>
2017-10-25 17:57:38 -0700

    added image output (also added png to requirements.txt)

--- cogs/ca.py
+++ cogs/ca.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
-import re
-import os
+import re, os
+import png
 
 # jesus christ i am sorry (matches B/S and if no B then either 2-state single-slash rulestring or generations rulestring)
 rrulestring = re.compile(r'^(B)?[0-8cekainyqjrtwz-]*/(?(1)S?[0-8cekainyqjrtwz\-]*|[0-8cekainyqjrtwz\-]*(?:/[\d]{1,3})?)$') 
@@ -12,18 +12,14 @@ rpattern = re.compile(r'^[\dobo$]*[obo$][\dobo$]*!?$|^[.*!]+$')
 # matches multiline XRLE
 rxrle = re.compile(r'^(?:#.*$)?(?:^x ?= ?\d+, ?y ?= ?\d+, ?rule ?= ?(.+)$)?\n(^[\dobo$]*[obo$][\dobo$\n]*!?)$', re.M)
 
+# splits RLE into its runs
+rruns = re.compile(r'([0-9]*)([ob])') # [rruns.sub(lambda m:''.join(['0' if m.group(2) == 'b' else '1' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[i]]
+
+# unrolls $ signs
+rdollarsigns = re.compile(r'(\d+)\$')
+
 # matches .lif
 rlif = re.compile(r'(?:^[.*!]+$)+')
-
-
-# runs of dots/stars
-rruns = re.compile(r'([0-9]+)([ob])') # rruns.sub(lambda m:''.join(['.' if m.group(2) == 'b' else '*' for x in range(int(m.group(1)))]), rle)
-
-# single dots/stars
-rsingletons = re.compile(r'(?<![0-9])[ob]') # singletons.sub(lambda m:'.' if m.group() == 'b' else '*', rle)
-
-# exclamation points
-rexclm = re.compile(r'([0-3]*)\$') # exclm.sub(lambda m:'!' if m.group(1) == '' else ''.join(['!' for x in range(int(m.group(1)))]), rle)
 
 
 # ---- #
@@ -39,6 +35,7 @@ class CA:
     
     @commands.command(name='sim')
     async def sim(self, ctx, *args): #args: *RULE *PAT GEN *STEP *g
+        os.mkdir(f'{self.dir}/{ctx.message.id}_frames')
         gfy = False
         if 'g' in args:
             args.pop(args.index('g'))
@@ -77,28 +74,32 @@ class CA:
                 return
         await ctx.send('Running supplied pattern in rule `{0[rule]}` with step `{0[step]}` until generation `{0[gen]}`.'.format(parse))
         
-        with open(f'{ctx.message.id}_in.rle', 'w') as pat:
+        with open(f'{self.dir}/{ctx.message.id}_in.rle', 'w') as pat:
             pat.write(parse["pat"])
         
-        filedir = os.path.dirname(os.path.abspath(f'{ctx.message.id}_in.rle'))
-        
-        os.system('{0}/resources/bgolly -m {1[gen]} -i {1[step]} -q -q -r {1[rule]} -o {0}/{2}_out.rle {3}/{2}_in.rle'.format(self.dir, parse, ctx.message.id, filedir))
+        os.system('{0}/resources/bgolly -m {1[gen]} -i {1[step]} -q -q -r {1[rule]} -o {0}/{2}_out.rle {0}/{2}_in.rle'.format(self.dir, parse, ctx.message.id))
         # From here:
         # readlines on bgolly's output file and divide resulting list into two - one with each individual RLE and one with corresponding (width, height)
         with open(f'{self.dir}/{ctx.message.id}_out.rle', 'r') as pat:
             patlist = [line.rstrip('\n') for line in pat]
+
+        os.remove(f'{self.dir}/{ctx.message.id}_out.rle')
+        os.remove(f'{self.dir}/{ctx.message.id}_in.rle')
         
         headers = patlist[::2] # just headers
         headers = [tuple(map(int, header[4:header.find(', r')].split(', y = '))) for header in headers] # remove rulestring (not needed) and convert x, y to tuple of ints
-        headers = [(x, y, x * y) for x, y in headers] # put total area in tuple[2]
         
         patlist = patlist[1::2] # just RLE
-        # patlist = [pattern[:pattern.find('!')] for pattern in patlist] # remove final exclamation point
-        
-        # applies above regexes to turn RLE into .lif ... readable enough if second arg recursively unpacked
-        # patlist = [rexclm.sub(lambda m:'!' if m.group(1) == '' else ''.join(['!' for x in range(int(m.group(1)))]), rsingletons.sub(lambda m:'.' if m.group() == 'b' else '*', rruns.sub(lambda m:''.join(['.' if m.group(2) == 'b' else '*' for x in range(int(m.group(1)))]), pattern))) for pattern in patlist]
-        
-        print(patlist)
+        # ['4b3$o', '3o2b'] -> ['4b$$$o', '3o2b']
+        patlist = [rdollarsigns.sub(lambda m: ''.join(['$' for i in range(int(m.group(1)))]), j).replace('!', '') for j in patlist] # unroll newlines
+        # ['4b$$$o', '3o2b'] -> [['4b', '', '', '', 'o'], ['3o', '2b']]
+        patlist = [i.split('$') for i in patlist]
+
+        gif = []
+        for index in range(len(patlist)):
+            # unroll RLE and convert to list of ints, 0=off and 1=on, then lastly pad out to proper width
+            frame = [l+['0']*(headers[index][0] - len(l)) for l in [list(map(int, i)) for i in [rruns.sub(lambda m:''.join(['0' if m.group(2) == 'b' else '1' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[index].split('$')]]]
+            png.from_array(frame, 'L').save(f'{self.dir}/{ctx.message.id}_frames/{index}.rle')
         
         # finally pass all created pics to imageio for conversion to gif
         # then either upload to gfycat or send directly to discord depending on presence of "g" flag
