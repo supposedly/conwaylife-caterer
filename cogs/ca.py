96808fd182084be74b6f36b23823f541d0e38351
Hadi <hdtrhn@gmail.com>
2017-10-25 18:32:12 -0700

    revert to old way to see if this even works anymore

--- cogs/ca.py
+++ cogs/ca.py
@@ -98,7 +98,7 @@ class CA:
         gif = []
         for index in range(len(patlist)):
             # unroll RLE and convert to list of ints, 0=off and 1=on, then lastly pad out to proper width
-            frame = [l+[0]*(headers[index][0] - len(l)) for l in [list(map(int, i)) for i in [rruns.sub(lambda m:''.join(['0' if m.group(2) == 'b' else '255' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[index]]]]
+            frame = [l+[0]*(headers[index][0] - len(l)) for l in [list(map(int, i)) for i in [rruns.sub(lambda m:''.join(['0' if m.group(2) == 'b' else '1' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[index]]]]
             png.from_array(frame, 'L').save(f'{self.dir}/{ctx.message.id}_frames/{index}.png')
         
         # finally pass all created pics to imageio for conversion to gif
