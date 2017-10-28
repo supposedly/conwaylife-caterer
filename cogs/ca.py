079bf9311c94a3b7699da4cbd53d4674ce631f86
Hadi <hdtrhn@gmail.com>
2017-10-27 18:44:46 -0700

    i have either (a) fixed padding entirely, or (b) fucked it up beyond repair that does not involve reverting commits

--- cogs/ca.py
+++ cogs/ca.py
@@ -107,10 +107,14 @@ class CA:
 
         for index in range(len(patlist)):
             # unroll RLE and convert to list of ints, 1=off and 0=on, then lastly pad out to proper width
-            frame = [l+[1]*((maxwidth - len(l)) + positions[index][0]) for l in [list(map(int, i)) for i in [rruns.sub(lambda m:''.join(['1' if m.group(2) == 'b' else '0' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[index]]]]
+            frame = [l+[1]*((maxwidth - len(l)) - positions[index][0]) for l in [list(map(int, i)) for i in [rruns.sub(lambda m:''.join(['1' if m.group(2) == 'b' else '0' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[index]]]]
             
-            # pad out to proper height with 1=off cell
-            [frame.append([1]*maxwidth) for j in range((maxheight - len(frame)) + positions[index][1])]
+            # pad out to proper width with 1=off cell
+            [frame.append([1]*maxwidth) for j in range((maxheight - len(frame)) - positions[index][1])]
+            
+            # pad beginning of strings
+            frame = [[1]*((maxwidth - len(l)))+l for l in frame]
+            [frame.insert(0, ([1]*maxwidth)) for j in range((maxheight - len(frame)))]
             
             # double frame size, need to find a better way than nested list comp to do this haha
             frame = [[[frame[i][j//2] for j in range(len(frame[i])*2)] for i in range(len(frame))][k//2] for k in range(len(frame)*2)]
