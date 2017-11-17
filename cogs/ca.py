450e271a058e2f03789f4676822d322a817c1b83
Eli <eltrhn@gmail.com>
2017-11-16 20:45:47 -0800

    fix patterns given as command argument, improve pattern searching in channel history

--- cogs/ca.py
+++ cogs/ca.py
@@ -12,11 +12,11 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 # matches LtL rulestring
 rLtL = re.compile(r'R\d{1,3},C\d{1,3},M[01],S\d+\.\.\d+,B\d+\.\.\d+,N[NM]')
 
-# jesus christ i am sorry (matches B/S and if no B then either 2-state single-slash rulestring or generations rulestring)
+# matches B/S and if no B then either 2-state single-slash rulestring or generations rulestring
 rrulestring = re.compile(r'(B)?[0-8cekainyqjrtwz-]+(?(1)/?(S)?[0-8cekainyqjrtwz\-]*|/(S)?[0-8cekainyqjrtwz\-]*(?(2)|(?(3)|/[\d]{1,3})?))')
 
-# matches multiline XRLE
-rxrle = re.compile(r'^(?:#.*$)?(?:^x ?= ?\d+, ?y ?= ?\d+(?:, ?rule ?= ?([^ \n]+))?)?\n(^[\dob$]*[ob$][\dob$\n]*!?)$', re.M)
+# matches multiline XRLE; currently cannot, however, match headerless patterns (my attempts thus far have forced re to take much too many steps)
+rxrle = re.compile(r'x ?= ?\d+, ?y ?= ?\d+(?:, ?rule ?= ?([^ \n]+))?\n([\dob$]*[ob$][\dob$\n]*!?)')
 
 # splits RLE into its runs
 rruns = re.compile(r'([0-9]*)([ob])') # [rruns.sub(lambda m:''.join(['0' if m.group(2) == 'b' else '1' for x in range(int(m.group(1)) if m.group(1) else 1)]), pattern) for pattern in patlist[i]]
@@ -107,13 +107,16 @@ def makegif(current, gen, step):
     # finally, pass all created pics to imageio for conversion to gif
     # then either upload to gfycat or send directly to discord depending on presence of "g" flag
     png_dir = f'{current}_frames/'
-    for subdir, dirs, files in os.walk(png_dir):
-        files.sort()
-        with imageio.get_writer(f'{current}.gif', mode='I', duration=str(max(1/60, (-0.5 / (-gen // 10)) / step))) as writer:
-            for file in files:
-                file_path = os.path.join(subdir, file)
-                writer.append_data(imageio.imread(file_path))
-    os.system(f'rm -r {png_dir}')
+    duration = max(1/60, (-0.5 / (-gen // 10)) / step) if gen else 1
+    try:
+        for subdir, dirs, files in os.walk(png_dir):
+            files.sort()
+            with imageio.get_writer(f'{current}.gif', mode='I', duration=str(duration)) as writer:
+                for file in files:
+                    file_path = os.path.join(subdir, file)
+                    writer.append_data(imageio.imread(file_path))
+    finally:
+        os.system(f'rm -r {png_dir}')
 
 def makesoup(rulestring, x, y):
     """generates random soup as RLE with specified dimensions"""
@@ -157,17 +160,16 @@ class CA:
             rand = f'Running `{dims}` soup in rule `{rule}` with step `{step}` until generation `{gen}`.' # meh
         if pat is None:
             async for msg in ctx.channel.history(limit=50):
-                try:
-                    rmatch = list(filter(None, [rxrle.match(i) for i in msg.content.split('`')]))[0]
-                except IndexError as e:
-                    pass
-                else:
+                rmatch = rxrle.search(msg.content)
+                if rmatch:
                     pat = rmatch.group(2)
                     if rmatch.group(1):
                         rule = rmatch.group(1)
                     break
             if pat is None: # stupid
                 return await ctx.send(f"`Error: No PAT given and none found in channel history. '{self.bot.command_prefix(self.bot, ctx.message)}help sim' for more info`")
+        else:
+            pat = pat.strip('`')
         await ctx.send(rand if rand else f'Running supplied pattern in rule `{rule}` with step `{step}` until generation `{gen}`.')
         
         with open(f'{current}_in.rle', 'w') as infile:
@@ -182,10 +184,11 @@ class CA:
         # create gif on separate process to avoid blocking event loop
         patlist, positions, bbox = await self.loop.run_in_executor(self.executor, parse, current)
         await self.loop.run_in_executor(self.executor, makeframes, current, patlist, positions, bbox, len(str(gen)))
-        await self.loop.run_in_executor(self.executor, makegif, current, gen, step)
-        
-        await ctx.send(file=discord.File(f'{current}.gif'))
-        os.remove(f'{current}.gif')
+        try:
+            await self.loop.run_in_executor(self.executor, makegif, current, gen, step)
+            await ctx.send(file=discord.File(f'{current}.gif'))
+        finally:
+            os.remove(f'{current}.gif')
         
     @sim.command(name='rand', aliases=cmd.aliases['sim.rand'])
     async def rand(self, ctx, x='', y='', gen='', rule='', step: int=1):
@@ -236,7 +239,7 @@ class CA:
             try:
                 print('Ignoring exception in on_message', exc[0].split('"""')[1], *exc[1:end])
             except Exception as e:
-                print(f'{e.__class__.__name__}: {e}\n\n')
+                print(f'{e.__class__.__name__}: {e}\n--------\n')
                 raise error
         else:
             raise error
